6b171d0d5d6a89a5116cce913a83e3ad148306c6
Michael Bonney <bonneym@flip3.engr.oregonstate.edu>
2016-11-23 15:26:16 -0800

    add js/css to communities.php

--- communities.php
+++ communities.php
@@ -9,11 +9,58 @@ if($mysqli->connect_errno) {
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="I'll Go">
+    <meta name="author" content="Group 15">
+    <link rel="icon" href="../../favicon.ico">
     <title>Opportunities</title>
-    <link rel="stylesheet" href="style.css" type="text/css">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/jumbotron.css" rel="stylesheet">
+    <link href="css/signin.css" rel="stylesheet">
+    <link href="css/sticky-footer-navbar.css" rel="stylesheet">
   </head>
   <body>
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <!--Do we have a JPG logo?-->
+          <a class="navbar-brand" href="index.php"><img src="images/logo.gif" width="75px" height="30px"></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <div>
+            <ul class="nav navbar-nav">
+              <li><a href="index.php">I'll Go</a></li>
+              <li><a href="communities.php">Communities</a></li>
+              <li><a href="something.php">Something</a></li>
+              <li><a href="somethingelse.php">Something Else </a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+            	<!--This is where we can to update Login/Logout features, profile page link, etc.-->
+              <?php
+            //   if(isset($_SESSION['sessionActive'])) {
+            //     echo "<li><a href='profile.php'><font color='#FDB829'>$_SESSION[firstname] $_SESSION[lastname]</font></a></li>";
+            //     echo "<li><a href='logoutAction.php?action=end'><span class='glyphicon glyphicon-log-out'></span> Logout</a></li>";
+            //   }
+            //   else {
+            //     echo "<li><a href='login.php'><span class='glyphicon glyphicon-log-in'></span> Login</a></li>";
+            //     echo "<li class='active'><a href='newaccount.php'><span class='glyphicon glyphicon-user'></span> Create Account</a></li>";
+            //   }
+              ?>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
     <div class="header">
       
       <h1>Opportunities</h1>
@@ -64,6 +111,6 @@ $statement->close();
 
 
 
-
+     <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
