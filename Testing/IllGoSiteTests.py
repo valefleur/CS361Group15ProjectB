beb0c39e1610dc04d9c51f4d936dee0df7f8fe2d
wiltseb <wiltseb@oregonstate.edu>
2016-11-26 08:16:25 -0800

    Fixed tests. 2 tests for both pages so far

--- Testing/IllGoSiteTests.py
+++ Testing/IllGoSiteTests.py
@@ -17,32 +17,42 @@ newAccountURL = baseURL + "newaccount.php"
 communitiesURL = baseURL + "communities.php"
 
 class NewAccountTestCases(unittest.TestCase):
-	"""Testing NewAccount.php page"""
-	@classmethod
-	def setUpClass(self):
-		self.firefox = webdriver.Firefox()
-		self.firefox.get(newAccountURL)
+        """Testing NewAccount.php page"""
+        @classmethod
+        def setUpClass(self):
+                self.firefox = webdriver.Firefox()
+                self.firefox.get(newAccountURL)
 
-	@classmethod
-	def tearDownClass(self):
-		time.sleep(5) #only so we can see what's happening
-		self.firefox.quit()
+        @classmethod
+        def tearDownClass(self):
+                time.sleep(5) #only so we can see what's happening
+                self.firefox.quit()
 
 
-	def test_new_account_page_is_accessible(self):
-		"""Is the new account page accessible?""" #this question gets printed on the console for easy reading
-		# Here is the SET UP code
-		WebDriverWait(self.firefox, 10).until(EC.title_contains("Create Account"))
-		self.assertIn("Create Account", self.firefox.title)
+        def test_new_account_page_is_accessible(self):
+                """Is the new account page accessible?""" #this question gets printed on the console for easy reading
+                # Here is the SET UP code
+                WebDriverWait(self.firefox, 10).until(EC.title_contains("Create Account"))
+                self.assertIn("Create Account", self.firefox.title)
 
-	def test_first_name_is_editable(self):
-		"""Can the first name field be edited?"""
-		textbox = self.firefox.find_element_by_id("inputFirstname")
-		time.sleep(1) #only so we can see what's happening
-		textbox.send_keys("Primary Name")
-		text = textbox.get_attribute("value")
-		self.assertIn("Primary Name", text)
-		
+        def test_first_name_is_editable(self):
+                """Can the first name field be edited?"""
+                textbox = self.firefox.find_element_by_id("inputFirstname")
+                time.sleep(1) #only so we can see what's happening
+                textbox.send_keys("Primary Name")
+                text = textbox.get_attribute("value")
+                self.assertIn("Primary Name", text)
+
+        def test_last_name_is_editable(self):
+                """Can the last name field be edited?"""
+                textbox = self.firefox.find_element_by_id("inputLastname")
+                time.sleep(1) #only so we can see what's happening
+                textbox.send_keys("Last Name")
+                text = textbox.get_attribute("value")
+                self.assertIn("Last Name", text)
+
+
+    
 class CommunitiesTestCases(unittest.TestCase):
         """Testing communities.php page"""
         @classmethod
@@ -64,12 +74,20 @@ class CommunitiesTestCases(unittest.TestCase):
 
         def test_city_is_editable(self):
                 """Can the city field be edited?"""
-                textbox = self.firefox.find_element_by_id("Name")  #misnomer for city variable in communities.php 
+                textbox = self.firefox.find_element_by_name("Name")  #misnomer for city variable in communities.php 
                 time.sleep(1) #only so we can see what's happening
                 textbox.send_keys("cityName")
                 text = textbox.get_attribute("value")
                 self.assertIn("cityName", text)
 
+        def test_state_is_editable(self):
+                """Can the State field be edited?"""
+                textbox = self.firefox.find_element_by_name("State")
+                time.sleep(1)
+                textbox.send_keys("stateName")
+                text = textbox.get_attribute("value")
+                self.assertIn("stateName", text)
+
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+        unittest.main(verbosity=2)
 
