cfe003280f92b4d3abd94140712e790ab2e5b0fc
CoreySavage <Savageco@oregonstate.edu>
2016-12-03 11:27:21 -0500

    Inserts info into Account_Community

---
+++ addVolunteer.php
@@ -0,0 +1,26 @@
+<?php
+ini_set('display_errors', 'On');
+$mysqli = new mysqli("oniddb.cws.oregonstate.edu","bonneym-db", "R2lzWpqYli7k9Qk8", "bonneym-db");
+
+if($mysqli->connect_error){
+    echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+
+$user = isset($_POST['AccountName']) ? $_GET['AccountName'] : '';
+$pass = isset($_POST['psw']) ? $_POST['psw'] : '';
+
+if(!($stmt = $mysqli->prepare("INSERT INTO `Account_Community`(`AccountID`, `CommunityID`, `Skill`, `StartDate`, `EndDate`) VALUES ((SELECT `AccountID` FROM `Account` WHERE (`UserName` = $user AND `Password` = $psw)),?,?,?,?)"))){
+    echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+}
+
+if(!($stmt->bind_param("isss",$_POST['CommunityID'],$_POST['CommunitySkill'],$_POST['startDate'],$_POST['endDate']))){
+	echo "Bind failed: "  . $stmt->errno . " " . $stmt->error;
+}
+
+if(!$stmt->execute()){
+   echo "Execute failed: "  . $stmt->errno . " " . $stmt->error;
+} else {
+	echo "Added volunteer to community.";    
+}
+    
+?>
