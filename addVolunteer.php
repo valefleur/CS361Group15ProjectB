b40419993b163b2df1691211371c7db9d01fd493
Micheal Willard <willardm@onid.oregonstate.edu>
2016-12-04 16:09:28 -0800

    Update addVolunteer.php

--- addVolunteer.php
+++ addVolunteer.php
@@ -13,7 +13,8 @@ $date1 = date('Y-m-d', strtotime($rawdate1));
 $rawdate2 = htmlentities($_POST['endDate']);
 $date2 = date('Y-m-d', strtotime($rawdate2));
 
-$result = $mysqli->prepare("SELECT AccountID FROM Account WHERE UserName = '$user' AND Password = '$pass'")
+$result = $mysqli->prepare("SELECT AccountID FROM Account WHERE UserName = '$user'")
+// 	 AND Password = '$pass'
 
 $result->execute();
 $result->bind_result($aid);
